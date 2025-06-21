538f49028040bb534ca0b607e805b7aaff992438
Christopher Debray <christopherdebray1@gmail.com>
2025-06-21 21:48:42 +0200

    feat: added casting spell strategy

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -17,6 +17,20 @@ enum ELEMENTS {
 	WIND,
 }
 
+var spell_book := {
+	[ELEMENTS.FIRE, ELEMENTS.FIRE]: "Fireball",
+	[ELEMENTS.WATER, ELEMENTS.WATER]: "Ice Lance",
+	[ELEMENTS.EARTH, ELEMENTS.EARTH]: "Stone Shield",
+	[ELEMENTS.WIND, ELEMENTS.WIND]: "Gust",
+
+	[ELEMENTS.FIRE, ELEMENTS.WATER]: "Steam Burst",
+	[ELEMENTS.FIRE, ELEMENTS.EARTH]: "Magma Wave",
+	[ELEMENTS.WATER, ELEMENTS.WIND]: "Blizzard",
+	[ELEMENTS.EARTH, ELEMENTS.WIND]: "Sandstorm",
+	[ELEMENTS.FIRE, ELEMENTS.WIND]: "Flame Tornado",
+	[ELEMENTS.WATER, ELEMENTS.EARTH]: "Mud Trap",
+}
+
 const SPEED: float = 230.0
 const PROBE_SIZE = Vector2(50, 50)
 
@@ -45,14 +59,17 @@ func detect_action_inputs() -> void:
 
 func detect_interaction_inputs() -> void:
 	if Input.is_action_just_pressed("x_1_action"):
-		activate_element(ELEMENTS.FIRE)
-	if Input.is_action_just_pressed("y_2_action"):
 		activate_element(ELEMENTS.WATER)
+	if Input.is_action_just_pressed("y_2_action"):
+		activate_element(ELEMENTS.FIRE)
 	if Input.is_action_just_pressed("b_3_action"):
-		activate_element(ELEMENTS.WIND)
+		activate_element(ELEMENTS.EARTH)
 	
 	if Input.is_action_just_pressed("validate"):
 		print("validate");
+	
+	if Input.is_action_just_pressed("use_spell"):
+		use_spell()
 
 func activate_element(element: ELEMENTS) -> void:
 	if active_elements.size() >= 2:
@@ -84,6 +101,36 @@ func _update_elements_display() -> void:
 		second_element.hide()
 		timer_second_element.stop()
 
+func get_aim_direction() -> Vector2:
+	if Input.get_connected_joypads().size():
+		var stick_direction: Vector2 = Vector2.RIGHT
+		stick_direction.x = Input.get_action_strength("aim_right") - Input.get_action_strength("aim_left")
+		stick_direction.y = Input.get_action_strength("aim_down") - Input.get_action_strength("aim_up")
+		return Vector2.ZERO.direction_to(stick_direction)
+	
+	return global_position.direction_to(get_global_mouse_position())
+
+func use_spell() -> void:
+	var spell_name = get_spell_from_elements(active_elements)
+	print("Casting spell:", spell_name)
+	_remove_element(1)
+	_remove_element(0)
+
+func get_spell_from_elements(elements: Array[int]) -> String:
+	if elements.is_empty():
+		return "No spell"
+
+	var combo = elements.duplicate()
+	combo.sort() # ignore l’ordre d’activation
+
+	if spell_book.has(combo):
+		return spell_book[combo]
+	
+	if combo.size() == 1 and spell_book.has([combo[0]]):
+		return spell_book[[combo[0]]]
+
+	return "Unknown combination"
+
 func _on_timer_first_element_timeout() -> void:
 	if active_elements.size() > 0:
 		_remove_element(0)
@@ -103,12 +150,3 @@ func _on_timer_second_element_timeout() -> void:
 	#
 	#if Input.is_action_just_pressed("switch_item_prev"):
 		#item_handler.switch_item(-1)
-#
-#func get_aim_direction() -> Vector2:
-	#if Input.get_connected_joypads().size():
-		#var stick_direction: Vector2 = Vector2.RIGHT
-		#stick_direction.x = Input.get_action_strength("aim_right") - Input.get_action_strength("aim_left")
-		#stick_direction.y = Input.get_action_strength("aim_down") - Input.get_action_strength("aim_up")
-		#return Vector2.ZERO.direction_to(stick_direction)
-	#
-	#return global_position.direction_to(get_global_mouse_position())
