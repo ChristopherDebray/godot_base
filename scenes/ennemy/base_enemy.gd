1c21bc602d8cebaf5cdc4260eda4b4aaaf339ae1
Christopher Debray <christopherdebray1@gmail.com>
2025-08-16 09:16:00 +0200

    feat: Added functional roaming state for base enemy

--- scenes/ennemy/base_enemy.gd
+++ scenes/ennemy/base_enemy.gd
@@ -11,32 +11,46 @@ class_name BaseEnemy extends Damageable
 @onready var ray_cast_2d: RayCast2D = $Detection/RayCast2D
 @onready var field_view: Area2D = $SpriteContainer/FieldView
 @onready var attack_timer: Timer = $AttackTimer
-@export var turn_speed_deg: float = 660.0 # vitesse de rotation du FOV (deg/sec)
 
-#distance_to_target to use for fleeing or on a different class for kitting
-#maybe use composition for behaviors ?
+@export var turn_speed_deg: float = 660.0 # FOV rotation speed (deg/sec)
 
 @export var attack_cooldown: float = 2.0
 @export var patrol_points: NodePath
 @export var initial_state: STATE = STATE.IDLE
 @export var behavior: EnemyBehavior
+@export var roam_radius: float = 200.0
 
-enum STATE { IDLE, RETURNING, ROAMING, PATROLLING, CHASING, ATTACKING, SEARCHING, FLEEING, DEAD }
+enum STATE {
+	IDLE,
+	## When enemy as search for the player but didn't found him, returns to his original point
+	RETURNING,
+	## Roam freely where it stands
+	ROAMING,
+	PATROLLING,
+	ATTACKING,
+	SEARCHING,
+	FLEEING,
+	DEAD
+}
 
 ## This timer will be based on provided attack cooldown and will vary dynamicly for a more organic attack pattern
 var next_attack_timer: float
 var state: STATE
+
 var _waypoints: Array = []
 var _current_wp: int = 0
+
 var _player_ref: Player
+
 var _initial_facing_direction: Vector2
 var _initial_position: Vector2
+
 var _player_in_fov: bool = false
 var _last_seen_pos: Vector2 = Vector2.ZERO
 
-##### @todo rotate l'enemy vers moi parce que sinon juste je quitte le champs de vision et cest fini quoi
-### Le souci c'est que dans mon autre jeu je faisais du calcul à la mano donc il pouvait gérer, plus maintenant
-### Estce que c'est mieux de faire comme l'autre jeu ? Mais du coup tu vois sur une distance de ouf 'fin niveau calcul etc c'est pas trop lourd
+var roaming_target_position: Vector2
+var roam_delay: float = 2.0
+var _roam_timer: float = 0.0
 
 func _ready() -> void:
 	setup()
@@ -50,6 +64,7 @@ func setup():
 	_initial_position = global_position
 	_create_wp()
 
+## A deffered setup for elements that need to be loaded before it
 func late_setup():
 	# This part is mandatory or else the physics_process movements occur before waypoints creations
 	await get_tree().physics_frame
@@ -58,22 +73,21 @@ func late_setup():
 
 func _physics_process(delta: float) -> void:
 	_refresh_fov_poll()
-	handle_player_detection()
-	update_action_by_state(delta)
+	_handle_player_detection()
+	_update_action_by_state(delta)
 	_update_navigation()
 	_update_facing(delta)  
 
-func handle_player_detection():
-	if not is_instance_valid(_player_ref):
-		return
+## Set the state and next position based on player _last_seen_pos and if the player is detected or not
+func _handle_player_detection():
 	if not _player_in_fov:
-		if state == STATE.CHASING or state == STATE.ATTACKING:
+		if state == STATE.ATTACKING:
 			state = STATE.SEARCHING
 			set_nav_to_position(_last_seen_pos)
 		return
 	
 	if not _check_raycast_to_player():
-		if state == STATE.CHASING or state == STATE.ATTACKING:
+		if state == STATE.ATTACKING:
 			state = STATE.SEARCHING
 			set_nav_to_position(_last_seen_pos)
 		return
@@ -82,16 +96,13 @@ func handle_player_detection():
 	if state == STATE.IDLE or state == STATE.RETURNING or state == STATE.ROAMING or state == STATE.PATROLLING or state == STATE.SEARCHING:
 		state = STATE.ATTACKING
 
+## Checks if user is in pov / field_view with overlaps_body, body_entered with area isn't enough
 func _refresh_fov_poll() -> void:
-	if not is_instance_valid(_player_ref):
-		_player_in_fov = false
-		return
 	_player_in_fov = field_view.overlaps_body(_player_ref)
 
+## Set the raycast to point to the player and check if there is no obstacle between
+## It is only used when player is on fov to avoid too many recalculation
 func _check_raycast_to_player() -> bool:
-	if not is_instance_valid(_player_ref):
-		return false
-	
 	ray_cast_2d.target_position = ray_cast_2d.to_local(_player_ref.global_position)
 	ray_cast_2d.force_raycast_update()
 	if not ray_cast_2d.is_colliding():
@@ -105,16 +116,14 @@ func _check_raycast_to_player() -> bool:
 
 	return true
 
-func update_action_by_state(delta: float):
+func _update_action_by_state(delta: float):
 	match state:
 		STATE.IDLE:
-			pass
+			process_idle()
 		STATE.RETURNING:
 			process_returning()
 		STATE.ROAMING:
-			process_roaming()
-		STATE.CHASING:
-			process_chasing(delta)
+			process_roaming(delta)
 		STATE.PATROLLING:
 			process_patrolling()
 		STATE.SEARCHING:
@@ -126,15 +135,17 @@ func update_action_by_state(delta: float):
 		STATE.FLEEING:
 			process_fleeing(delta)
 		STATE.DEAD:
-			# Should not exist
-			pass
+			process_dead()
 
-func process_chasing(delta: float):
-	set_nav_to_position(_player_ref.global_position)
+## @abstract
+func process_idle() -> void:
+	pass
 
+## @abstract
 func process_attacking(delta: float):
 	set_nav_to_position(_player_ref.global_position)
 
+## @abstract
 func process_searching(delta: float):
 	if not nav_agent.is_navigation_finished():
 		return
@@ -171,7 +182,6 @@ func set_next_attack_timer():
 
 ## @abstract
 func process_fleeing(delta):
-	# @todo add if attacking can't move ?
 	var away = (global_position - _player_ref.global_position).normalized()
 	var random_offset = Vector2(randf_range(-0.5, 0.5), randf_range(-0.5, 0.5)).normalized()
 	var flee_direction = (away + random_offset).normalized()
@@ -179,18 +189,36 @@ func process_fleeing(delta):
 	nav_agent.target_position = global_position + flee_direction * 200
 
 ## @abstract
-func process_roaming() -> void:
-	pass
+func process_roaming(delta: float) -> void:
+	if not nav_agent.is_navigation_finished():
+		return
+	
+	# cooldown before new destination
+	_roam_timer -= delta
+	if _roam_timer > 0.0:
+		return
+	
+	# choose a random point around position
+	var random_offset = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized() * randf_range(50.0, roam_radius)
+	roaming_target_position = _initial_position + random_offset
+	
+	set_nav_to_position(roaming_target_position)
+	_roam_timer = roam_delay + randf_range(0.5, 1.5) # variation for organic effectd
 
 ## @abstract
 func process_patrolling() -> void:
 	if nav_agent.is_navigation_finished() == true:
 		_navigate_wp()
 
+## @abstract
 func process_returning() -> void:
 	nav_agent.target_position = _initial_position
 	if nav_agent.is_navigation_finished() == true:
 		state = STATE.IDLE
+
+## @abstract
+func process_dead() -> void:
+	pass
 
 func _update_navigation() -> void:
 	var next_vel := Vector2.ZERO
@@ -214,12 +242,13 @@ func set_nav_to_position(nav_position: Vector2) -> void:
 	nav_agent.target_position = nav_position
 
 func _create_wp() -> void:
-	if state == STATE.IDLE:
+	if state != STATE.PATROLLING:
 		return
 	
 	for wp in get_node(patrol_points).get_children():
 		_waypoints.append(wp.global_position)
 
+## Set the nav_agent target to the next waypoint and goes reset to 0 if "parcour" is done
 func _navigate_wp() -> void:
 	if _current_wp >= len(_waypoints):
 		_current_wp = 0
@@ -239,32 +268,33 @@ func _rotate_node_towards(node: Node2D, target: Vector2, delta: float) -> void:
 	node.global_rotation = lerp_angle(current, desired, turn)
 
 func _update_facing(delta: float) -> void:
-	if not is_instance_valid(_player_ref):
-		return
-	
-	# 1. Combat, turns to playerd
-	if (state == STATE.CHASING or state == STATE.ATTACKING) and _player_in_fov and _check_raycast_to_player():
-		_rotate_node_towards(sprite_container, _player_ref.global_position, delta)
+	# 1. Combat, turns to player
+	if state == STATE.ATTACKING and _player_in_fov and _check_raycast_to_player():
+		_rotate_node_towards(self, _player_ref.global_position, delta)
 		return
 	
 	# 2. Search turns to last_seen_pos
 	if state == STATE.SEARCHING:
-		_rotate_node_towards(sprite_container, _last_seen_pos, delta)
+		_rotate_node_towards(self, _last_seen_pos, delta)
 		return
 	
 	if state == STATE.RETURNING:
-		_rotate_node_towards(sprite_container, _initial_position, delta)
+		_rotate_node_towards(self, _initial_position, delta)
 		return
 
 	# 3. Patrol rotates toward the next point
 	if state == STATE.PATROLLING and _waypoints.size() > 0:
 		var target = _waypoints[_current_wp - 1] if _current_wp > 0 else _waypoints[0]
-		_rotate_node_towards(sprite_container, target, delta)
+		_rotate_node_towards(self, target, delta)
+		return
+	
+	if state == STATE.ROAMING:
+		_rotate_node_towards(self, roaming_target_position, delta)
 		return
 	
 	# 4. Idle / return → keep initial rotation
 	if state == STATE.IDLE:
-		_rotate_node_towards(sprite_container, global_position + _initial_facing_direction, delta)
+		_rotate_node_towards(self, global_position + _initial_facing_direction, delta)
 
 
 func _on_nav_agent_velocity_computed(safe_velocity: Vector2) -> void:
