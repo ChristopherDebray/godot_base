ec2e8896e3737bad2665530b1d333bd9568f6260
Christopher Debray <christopherdebray1@gmail.com>
2025-09-12 09:34:06 +0200

    fix: kamikaze behavior and refactor folders / files to only use npc instead of enemy

--- scenes/npc/ennemy/skelleton/skelleton.gd
+++ scenes/npc/ennemy/skelleton/skelleton.gd
@@ -3,9 +3,8 @@ extends BaseNpc
 const ARROW_DATA: AbilityData = preload("res://data/abilities/common/arrow.tres")
 
 func _do_attack(delta: float) -> bool:
-	#instance, target, origin
-	var target = perception._attack_target.global_position
+	var target = _attack_target.global_position
 	var origin = muzzle.global_position
 	var direction = origin.direction_to(target)
-	SignalManager.use_ability.emit(ARROW_DATA, direction, origin, current_attack_target_type, self)
+	SignalManager.use_ability.emit(ARROW_DATA, direction, origin, targeting.current_attack_target_type, self)
 	return true
